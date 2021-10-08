30723c231d5d85b9d8ee45601c01835ca0178ec3
Paul <paul221122112211@gmail.com>
2021-10-08 08:58:09 +0200

    création TP2

---
+++ TP2_Bieres_THOMAS_GODARD/src/tp2_bieres_thomas_godard/BouteilleBiere.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tp2_bieres_thomas_godard;
+
+/**
+ *
+ * @author danielthomas
+ */
+public class BouteilleBiere {
+//    Nom : le nom de la bière, sous forme de String
+//- degreAlcool : son degré d’alcool sous forme de float
+//- brasserie : le nom de brasserie qui la produit, sous forme de String
+//- ouverte : il s’agit d’un booléen indiquant si la bouteille a été ouverte (la valeur de ouverte sera
+//à vrai), ou non (la valeur de ouverte sera à faux)
+    
+   String Nom;
+   float degreAlcool;
+   String brasserie;
+   boolean ouverte;
+}
