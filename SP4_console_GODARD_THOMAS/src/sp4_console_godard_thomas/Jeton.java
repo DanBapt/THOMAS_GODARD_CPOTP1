f7602c79de773e1951c40911bfb1072e014d1268
Paul <paul221122112211@gmail.com>
2021-11-12 15:06:15 +0100

    modif class jeton

--- SP4_console_GODARD_THOMAS/src/sp4_console_godard_thomas/Jeton.java
+++ SP4_console_GODARD_THOMAS/src/sp4_console_godard_thomas/Jeton.java
@@ -2,6 +2,13 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
+
+
+//Attributs :
+//• Couleur : couleur affectée au joueur Méthodes :
+//• Jeton (String) : constructeur initialisant la couleur du jeton avec le paramètre
+//• lireCouleur():renvoie la couleur du jeton
+
 package sp4_console_godard_thomas;
 
 /**
@@ -10,4 +17,15 @@ package sp4_console_godard_thomas;
  */
 public class Jeton {
     String Couleur;
-}
+    
+    public Jeton (String uneCouleur){
+        Couleur= uneCouleur;
+    }
+    public String lireCouleur(){
+//      renvoie la couleur du jeton
+    String chainearetourner;
+    chainearetourner = "la couleur est" + Couleur;
+    return chainearetourner;
+    }
+} 
+        
