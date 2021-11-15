97d2facc81ad3c1ed6b6cbfe9722a5caf076adb4
33666 <33666@LAPTOP-5OD536EM>
2021-11-15 17:15:46 +0100

    grille  méthodes

--- SP4_console_GODARD_THOMAS/src/sp4_console_godard_thomas/Grille.java
+++ SP4_console_GODARD_THOMAS/src/sp4_console_godard_thomas/Grille.java
@@ -9,5 +9,49 @@ package sp4_console_godard_thomas;
  * @author danielthomas
  */
 public class Grille {
-    Cellule tab[][]=new Cellule[6][7]; 
+    Cellule CelluleJeu[][]=new Cellule[6][7];
+    public void Grille(){
+        CelluleJeu=new Cellule [6][7];
+    }
+    public boolean ajouterJetonDansColonne(Jeton jtn,int x){
+        
+    }
+    public boolean etreRemplie(){
+        for (int i=0;i<6;i++){
+            if (CelluleJeu[6][i]!=null){
+                return true;
+            }
+        }
+        return false;
+    }
+    public void viderGrille(){
+        CelluleJeu[][]=null;
+    }
+    public void afficherGrilleSurConsole(){
+        
+    }
+    public boolean celluleOccupee(int x,int y){
+        
+    }
+    public String lireCouleurDuJeton(int x, int y){
+        
+    }
+    public boolean etreGagnantePourJoueur(Joueur joueur){
+        
+    }
+    public void tasserGrille(int a){
+        
+    }
+    public boolean colonneRemplie(int b){
+        
+    }
+    public boolean placerDesintegrateur(int x, int y){
+        
+    }
+    public boolean placerTrouNoir(int x,int y){
+        
+    }
+    public boolean supprimerJeton(int x,int y){
+        
+    }
 }
