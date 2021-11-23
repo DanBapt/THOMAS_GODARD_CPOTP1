cb399174a40b41c289a3ee9c5816ad8116a06a5b
33666 <33666@LAPTOP-5OD536EM>
2021-11-23 23:16:22 +0100

    modif class grille cellule et grille

--- SP4_console_GODARD_THOMAS/src/sp4_console_godard_thomas/Cellule.java
+++ SP4_console_GODARD_THOMAS/src/sp4_console_godard_thomas/Cellule.java
@@ -31,7 +31,7 @@ public class Cellule {
           return(jetonCourant);
     }
     public boolean supprimerJeton(){
-        if(jetonCourant!=null) {
+        if(jetonCourant!=null) { //danib
 
             jetonCourant=null;
 
