46f126c8f167caef3e16f0f268e20d54eb0ce102
Paul <paul221122112211@gmail.com>
2021-12-17 09:25:18 +0100

    création jeu

---
+++ Lights_out/src/lights_out/Lights_out.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package lights_out;
+
+/**
+ *
+ * @author danielthomas
+ */
+public class Lights_out {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    Grille g=new Grille();
+    }
+    
+}
