e006b7187e0f8f760ecd650e16bed44db40f956b
33666 <33666@LAPTOP-5OD536EM>
2021-12-17 10:43:26 +0100

    début methode grille

--- Lights_out/src/lights_out/Grille.java
+++ Lights_out/src/lights_out/Grille.java
@@ -9,7 +9,23 @@ package lights_out;
  * @author danielthomas
  */
 class Grille {
+    Bouton [][] BoutonJeu=new Bouton[5][5];
     public Grille(){
-    
-}
+        for (int i=0;i<5;i++){
+            for (int j=0;j<5;j++){
+                BoutonJeu[i][j]=new Bouton();
+            }
+        }
+    }
+    public boolean etreGagnant(){
+        for (int i=0;i<5;i++){
+            for (int j=0;i<5;j++){
+                if (BoutonJeu[i][j].couleur=="blanc"){
+                    return false;
+                }
+        if (BoutonJeu)
+                }
+            }
+        }
+    }
 }
